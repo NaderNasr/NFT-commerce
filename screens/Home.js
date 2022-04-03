09caeacf98d6a62f43d59e804d9b1378685f5626
NaderNasr <nadernasr7@gmail.com>
2022-04-02 20:58:47 -0400

    home screen setup and render titles from data

--- screens/Home.js
+++ screens/Home.js
@@ -1,9 +1,29 @@
-import {Text} from 'react-native'
+import { useState } from 'react-native';
+import { View, SafeAreaView, FlatList, Text } from 'react-native';
+import { COLORS, NFTData } from '../constants';
+import { NFTCard, HomeHeader, FocusedStatusBar } from '../components'
 
 const Home = () => {
   return (
-    <Text>Home</Text>
+    <SafeAreaView style={{ flex: 1 }}>
+      <FocusedStatusBar background={COLORS.primary}/>
+      <View style={{flex: 1}}>
+        <View style={{zIndex: 0}}>
+          <FlatList
+            data={NFTData}
+            renderItem={({ item }) => <Text>{item.name}</Text>}
+            keyExtractor={(item) => item.id}
+            showsVerticalScrollIndicator={false} //hide vertical scroll bar
+            ListHeaderComponent={<HomeHeader/>}
+
+          />
+        </View>
+      </View>
+    </SafeAreaView>
   )
 }
 
 export default Home
+
+//-----------------------------------------
+//https://reactnative.dev/docs/flatlist
